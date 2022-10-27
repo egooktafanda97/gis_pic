a92dc723c6aaed6b9715037ac8ae8aa4f4353104
egooktafanda97 <egooktafanda1097@gmail.com>
2022-10-27 11:44:55 +0700

    p-1

--- assets/website/apps/src/page/Gis/MapBox/CardTabItem.jsx
+++ assets/website/apps/src/page/Gis/MapBox/CardTabItem.jsx
@@ -14,7 +14,18 @@ export default function CardTabItem(props) {
 				{console.log(data)}
 				<ul className="tabs__list">
 					<li className="tabs__item tabs__item--active">
-						<a>{data?.dataSektor ?? ""}</a>
+						<a>
+							<div
+								className="w-100"
+								style={{
+									display: "flex",
+									justifyContent: "center",
+									alignItems: "center",
+								}}
+							>
+								<strong>{data?.dataSektor ?? ""}</strong>
+							</div>
+						</a>
 					</li>
 				</ul>
 				<div className="tabs__content">
@@ -22,8 +33,50 @@ export default function CardTabItem(props) {
 						<table className="table table-bordered">
 							<thead>
 								<tr>
-									<th scope="col">Nama Industri</th>
-									<th scope="col">{data?.nama_industri ?? ""}</th>
+									<th scope="col">Nama {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.nama_industri ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">Jenis {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.dataSektor ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">Kategori {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.pic_category_name ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">Tipe {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.pic_industry_type_name ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">Sektor {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.nama_sektor_utama_industri ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">Sub Sektor {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.nama_subsektor_industri ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">Nama Pemilik {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.nama_pemilik_industri ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">Perizinan {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.perizinan_industri ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">
+										besar_modal_industri {data?.dataSektor ?? ""}
+									</th>
+									<td scope="col">{data?.besar_modal_industri ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">Amalat {data?.dataSektor ?? ""}</th>
+									<td scope="col">{data?.alamat_industri ?? ""}</td>
+								</tr>
+								<tr>
+									<th scope="col">No Telepon Pemilik</th>
+									<td scope="col">{data?.telp_pemilik_industri ?? ""}</td>
 								</tr>
 							</thead>
 						</table>
